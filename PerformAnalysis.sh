c91c11b106d0ff7186252948c412e207305e35ef
Jack Anthony <anthony@hep.phy.cam.ac.uk>
2018-10-15 12:58:34 +0100

    Updated scripts.

--- PerformAnalysis.sh
+++ PerformAnalysis.sh
@@ -27,6 +27,15 @@ function EchoError() {
 
 function RunCommand() {
     local cmd=$1
+
+    if ! eval "$cmd"; then
+        EchoError "Command failed: ${LIGHT_GREY}$cmd"
+        exit 1
+    fi
+}
+
+function RunCommandAndPipe() {
+    local cmd=$1
     local logFile=$2
     local errFile=$3
     local fullCmd="$cmd > $logFile 2> $errFile"
@@ -37,29 +46,29 @@ function RunCommand() {
     fi
 }
 
-# Issue welcome
-cat << "EOF"
-                                          ,--,                        
-                                       ,---.'|                        
-   ,---,                               |   | :       ,---,.    ,---,. 
-  '  .' \                              :   : |     ,'  .' |  ,'  .' | 
- /  ;    '.          ,---,             |   ' :   ,---.'   |,---.'   | 
-:  :       \     ,-+-. /  |            ;   ; '   |   |   .'|   |   .' 
-:  |   /\   \   ,--.'|'   |  ,--.--.   '   | |__ :   :  |-,:   :  |-, 
-|  :  ' ;.   : |   |  ,"' | /       \  |   | :.'|:   |  ;/|:   |  ;/| 
-|  |  ;/  \   \|   | /  | |.--.  .-. | '   :    ;|   :   .'|   :   .' 
-'  :  | \  \ ,'|   | |  | | \__\/: . . |   |  ./ |   |  |-,|   |  |-, 
-|  |  '  '--'  |   | |  |/  ," .--.; | ;   : ;   '   :  ;/|'   :  ;/| 
-|  :  :        |   | |--'  /  /  ,.  | |   ,/    |   |    \|   |    \ 
-|  | ,'        |   |/     ;  :   .'   \'---'     |   :   .'|   :   .' 
-`--''          '---'      |  ,     .-./          |   | ,'  |   | ,'   
-                           `--`---'              `----'    `----'    
+# # Issue welcome
+# cat << "EOF"
+#                                           ,--,                        
+#                                        ,---.'|                        
+#    ,---,                               |   | :       ,---,.    ,---,. 
+#   '  .' \                              :   : |     ,'  .' |  ,'  .' | 
+#  /  ;    '.          ,---,             |   ' :   ,---.'   |,---.'   | 
+# :  :       \     ,-+-. /  |            ;   ; '   |   |   .'|   |   .' 
+# :  |   /\   \   ,--.'|'   |  ,--.--.   '   | |__ :   :  |-,:   :  |-, 
+# |  :  ' ;.   : |   |  ,"' | /       \  |   | :.'|:   |  ;/|:   |  ;/| 
+# |  |  ;/  \   \|   | /  | |.--.  .-. | '   :    ;|   :   .'|   :   .' 
+# '  :  | \  \ ,'|   | |  | | \__\/: . . |   |  ./ |   |  |-,|   |  |-, 
+# |  |  '  '--'  |   | |  |/  ," .--.; | ;   : ;   '   :  ;/|'   :  ;/| 
+# |  :  :        |   | |--'  /  /  ,.  | |   ,/    |   |    \|   |    \ 
+# |  | ,'        |   |/     ;  :   .'   \'---'     |   :   .'|   :   .' 
+# `--''          '---'      |  ,     .-./          |   | ,'  |   | ,'   
+#                            `--`---'              `----'    `----'    
 
-EOF
+# EOF
 
-EchoMessage "MicroBooNE LEE analysis steering scripts" $LIGHT_GREY_BOLD
-EchoMessage "Author: Jack Anthony <anthony@hep.phy.cam.ac.uk>\n" $LIGHT_GREY
-sleep 2
+# EchoMessage "MicroBooNE LEE analysis steering scripts" $LIGHT_GREY_BOLD
+# EchoMessage "Author: Jack Anthony <anthony@hep.phy.cam.ac.uk>\n" $LIGHT_GREY
+# sleep 2
 
 # Set up
 cwd=$(pwd)
@@ -85,9 +94,9 @@ mkdir -p $outputDir
 # Produce the energy estimator training sets
 energyEstimatorTrainingDir=$outputDir/EnergyEstimatorTrainingOutput
 EchoMessage "Writing energy estimator training sets: ${LIGHT_GREY}$energyEstimatorTrainingDir"
-RunCommand "pndr event_lists/events.txt $energyEstimatorTrainingDir ./settings/ProduceEnergyEstimatorTrainingSets.xml -n50" $logFile $errFile
+RunCommand "pndr event_lists/events.txt $energyEstimatorTrainingDir ./settings/ProduceEnergyEstimatorTrainingSets.xml -n10" $logFile $errFile
 
 #mkdir $energyEstimatorTrainingDir
-#valgrind --tool=massif pandora -i ./.settings.tmp.xml -n160 -pN -rFull
-#valgrind --num-callers=30 --suppressions=$ROOTSYS/etc/valgrind-root.supp pandora -i ./.settings.tmp.xml -n1 -pN -rFull
+#valgrind --tool=massif pandora -i ./.settings.tmp.xml -n10 -pN -rFull
+#valgrind --num-callers=30 --log-file=valgrind.log --suppressions=$ROOTSYS/etc/valgrind-root.supp pandora -i ./.settings.tmp.xml -n10 -pN -rFull
 
